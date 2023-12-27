59cf9e09b8115d5b7ca66171f306eac638b33b25
OrbitalBrain <154285451+OrbitalBrain@users.noreply.github.com>
2023-12-27 06:18:28 +0000

    Added SlashCommand Type

---
+++ src/types/types.ts
@@ -0,0 +1,36 @@
+import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
+
+export class SlashCommand extends SlashCommandBuilder {
+
+    private _guilds?: string[];
+
+    constructor() {
+        super();
+    }
+
+    /**
+     * Function to execute when the command is triggered
+     * @param interaction The interaction that triggered the command
+     * @returns A promise that resolves when the command is finished executing
+     */
+    public execute(interaction: ChatInputCommandInteraction): Promise<unknown> {
+        throw new Error("Method not implemented.");
+    }
+
+    /**
+     * The guilds that the command is available in
+     * @returns An array of guild ids
+     */
+    public get guilds(): string[] | undefined {
+        return this._guilds;
+    }
+
+    /**
+     * Set the guilds that the command is available in
+     * @param guilds An array of guild ids
+     */
+    public set guilds(guilds: string[] | undefined) {
+        this._guilds = guilds;
+    }
+
+}

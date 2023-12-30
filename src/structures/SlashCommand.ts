80034444492372524bee0650a975b1436eb23f9e
OrbitalBrain <154285451+OrbitalBrain@users.noreply.github.com>
2023-12-30 09:24:42 +0000

    Guild specific commands are now supported

--- src/structures/SlashCommand.ts
+++ src/structures/SlashCommand.ts
@@ -6,9 +6,9 @@ import { CommandInteraction, SlashCommandBuilder } from "discord.js";
 export class SlashCommand extends SlashCommandBuilder {
 
     /**
-     * The guilds that the command is available in (Not implemented yet)
+     * The guilds that the command is available in
      */
-    private _guilds?: string[];
+    public guilds?: string[];
     /**
      * Whether the command is enabled
      * @defaultValue true 
@@ -30,22 +30,6 @@ export class SlashCommand extends SlashCommandBuilder {
     public async execute(interaction: CommandInteraction): Promise<unknown> {
         await interaction.reply("This command has not been implemented yet!");
         throw new Error("Method not implemented.");
-    }
-
-    /**
-     * The guilds that the command is available in
-     * @returns An array of guild ids
-     */
-    public get guilds(): string[] | undefined {
-        return this._guilds;
-    }
-
-    /**
-     * Set the guilds that the command is available in
-     * @param guilds An array of guild ids
-     */
-    public set guilds(guilds: string[] | undefined) {
-        this._guilds = guilds;
     }
 
 }

8b81dcfecf6fe023d81b67bce731486ac11db2a5
x404dev <154285451+OrbitalBrain@users.noreply.github.com>
2024-04-25 02:15:19 -0400

    Initial Commit

--- docs/generate.ts
+++
@@ -1,7 +0,0 @@
-import { runGenerator } from '@discordjs/ts-docgen';
-
-runGenerator({
-  existingOutput: '.tmp/typedoc-out.json',
-  custom: 'docs/index.yml',
-  output: 'docs/docs.json'
-});
--- src/index.ts
+++
@@ -1,7 +0,0 @@
-export { SlashCtrl } from './SlashCtrl'
-
-export { SlashCommand } from './structures/SlashCommand';
-
-export { ButtonUtils } from './utils/ButtonUtils';
-
-export * from './types/types';

b625dbba579ac1281b15b6f2dba32c27b599afb2
x404dev <154285451+OrbitalBrain@users.noreply.github.com>
2024-04-25 02:04:40 -0400

    Started new docs

---
+++ docs/generate.ts
@@ -0,0 +1,7 @@
+import { runGenerator } from '@discordjs/ts-docgen';
+
+runGenerator({
+  existingOutput: '.tmp/typedoc-out.json',
+  custom: 'docs/index.yml',
+  output: 'docs/docs.json'
+});
--- src/SlashCtrl.ts
+++ src/SlashCtrl.ts
@@ -4,47 +4,20 @@ import { BaseInteraction, Collection, CommandInteraction, REST, Routes } from "d
 import { SlashCommand } from "./structures/SlashCommand";
 import { SlashCtrlSettings } from "./types/types";
 
-/**
- * The SlashCtrl class
- * @class SlashCtrl
- */
 class SlashCtrl {
 
-    /**
-     * The application id of the bot
-     * @private
-     * @readonly
-     * @type {string}
-     */
+
     private readonly _applicationId: string;
-    /**
-     * The token of the bot
-     * @private
-     * @readonly
-     * @type {string}
-     */
+
     private readonly _token: string;
 
-    /**
-     * The commands
-     * @private
-     * @type {Collection<string, SlashCommand>}
-     */
     private _commands: Collection<string, SlashCommand>;
 
-    /**
-     * Create a new SlashCtrl instance
-     * @param {SlashCtrlSettings} settings The settings for the SlashCtrl instance
-     */
     constructor(settings: SlashCtrlSettings) {
         this._applicationId = settings.applicationId;
         this._token = settings.token;
     }
 
-    /**
-     * Publish commands from a folder
-     * @param commandsPath The path to the folder containing the commands
-     */
     public async publishCommandsFromFolder(commandsPath: string) {
         const commandFiles = fs
             .readdirSync(commandsPath)
@@ -58,10 +31,6 @@ class SlashCtrl {
         this.publishCommands(commands);
     }
 
-    /**
-     * Publish commands
-     * @param commands The commands to publish
-     */
     public async publishCommands(commands: Collection<string, SlashCommand>) {
         if (commands.size === 0) return;
         this._commands = commands;
@@ -96,32 +65,18 @@ class SlashCtrl {
         }
     }
 
-    /**
-     * Handle commands
-     * @param interaction The interaction to handle
-     * @deprecated Use handleInteractions instead
-     */
     public handleCommands(interaction: BaseInteraction) {
         if (interaction.isCommand()) {
             this.executeCommand(interaction as CommandInteraction);
         }
     }
 
-    /**
-     * Handle interactions
-     * @param interaction The interaction to handle
-     */
     public s(interaction: BaseInteraction) {
         if (interaction.isCommand()) {
             this.executeCommand(interaction as CommandInteraction);
         }
     }
 
-    /**
-     * Execute a command
-     * @param interaction The interaction that triggered the command
-     * @private
-     */
     private executeCommand(interaction: CommandInteraction) {
         const command = this.commands.get(interaction.commandName);
         if (!command)
